14ac9c2c08f1ad650f2789cb6152516973550120
Pau Andrio <andriopau@gmail.com>
2023-06-27 12:00:46 +0200

    biobb_structure_checking biopython>=1.79

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## v3.13.1 (2023.2)
+## v3.13.2 (2023.2)
 ### Extended functions
   - input
     - Added assembly download from wwpdb.
--- biobb_structure_checking/constants.py
+++ biobb_structure_checking/constants.py
@@ -9,7 +9,7 @@ from os.path import join as opj
 
 from biobb_structure_checking.io.param_input import Dialog
 
-VERSION = '3.13.1'
+VERSION = '3.13.2'
 
 # Default locations and settings
 DATA_DIR_DEFAULT_PATH = 'dat'
